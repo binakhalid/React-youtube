17a9f9ddb414b628d7c4e966669397fcde85e2d6
Bina Khalid <binaanwar@gmail.com>
2017-09-22 11:51:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const API_KEY = 'AIzaSyD-Ln5z_lvO7LwJSucxwsIIQoEmtKcOLbY';
+ReactDOM.render (
+<App />,document.getElementById('root')
+);
